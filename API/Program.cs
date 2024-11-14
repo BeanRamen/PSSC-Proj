c8808f2d13bdc2e20a6a4b985b1b7ddf9e2bd78e
BeanRamen <pavelroscawow@gmail.com>
2024-11-14 10:04:25 +0200

    labspt7

---
+++ API/Program.cs
@@ -0,0 +1,56 @@
+using lab3.Models;
+using lab3.Repos;
+using lab3.Workflows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PricesContext = lab3.Repos.PricesContext;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContext<PricesContext>
+                (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddTransient<IPricesRepository, PricesRepository>();
+            builder.Services.AddTransient<ICartsRepository, CartsRepository>();
+            builder.Services.AddTransient<PublishItemWorkflow>();
+
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Example.Api", Version = "v1" });
+            });
+
+
+            WebApplication app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
--- lab3/obj/Debug/net8.0/lab3.AssemblyInfo.cs
+++ lab3/obj/Debug/net8.0/lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6f816068175918470d173dea50fdcf25b180b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d622efd9b13ce8b88d9d74a10a1d60d4172fd75")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
